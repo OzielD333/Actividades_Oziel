03eb75f0c3c2a321ab0d9541107e996cf4a64351
OzielD333 <26150503@aguascalientes.tecnm.mx>
2026-07-29 12:48:40 -0600

    Add files via upload

---
+++ Actividad_Selectiva/Act 2 trabajo 5.py
@@ -0,0 +1,15 @@
+celcius = float(input("Temperatura en °C: "))
+print("1. Fahrenheit\n2. Kelvin")
+opcion = int(input("Elige opción: "))
+match opcion:
+    case 1:
+        resultado = celcius * 9 / 5 + 32
+        unidad = "°F"
+    case 2:
+        resultado = celcius + 273.15
+        unidad = "K"
+    case _:
+        resultado = None
+        print("Opción inválida")
+if resultado is not None:
+    print("Convertido:", resultado, unidad)
